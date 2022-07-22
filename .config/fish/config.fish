15b570f243ab1ae1567c1f1e56bd22f1b2b2408d
astsu <you@example.com>
2022-07-22 09:32:46 +0300

    Removed unused aliases from fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,7 @@ set -gx fish_greeting 'Hi, bro! You\'re the best!'
 # Aliases
 alias to=cd
 alias lr=clear
-alias v=nvim
-alias vim=nvim
-alias oldvim="\vim"
-alias ccat=pygmentize
 alias lock="swaylock -c 000000"
-alias hx=helix
 
 # Pyenv init
 status is-login; and pyenv init --path | source
