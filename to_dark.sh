7a5ab914570c71d8fcad93ba36eb1bf3f7d893f4
astsu <you@example.com>
2022-07-24 21:43:41 +0300

    Add dark/light theme switcher.

---
+++ to_dark.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Waybar
+cp ~/.config/waybar/themes/macchiato.css ~/.config/waybar/style.css
+# Wallpaper
+cp ~/wallpapers/ghost-carrot-catppuccin-macchiato.png ~/wallpapers/wallpaper.png
+# Sway
+sed -i 's/include themes\/latte/include themes\/macchiato/' ~/.config/sway/config && swaymsg reload
+# kitty
+sed -i 's/include themes\/latte.conf/include themes\/macchiato.conf/' ~/.config/kitty/kitty.conf && kitty @ set-colors --all --configured ~/.config/kitty/kitty.conf
+# fish
+cp ~/.config/fish/mythemes/macchiato.fish ~/.config/fish/conf.d/theme.fish
+# LunarVim
+sed -i 's/vim.g.catppuccin_flavour \= "latte"/vim.g.catppuccin_flavour = "macchiato"/' ~/.config/lvim/config.lua
+# mako
+cp ~/.config/mako/macchiato ~/.config/mako/config && makoctl reload
+# wofi
+cp ~/.config/wofi/macchiato.css ~/.config/wofi/style.css
---
+++ to_light.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Waybar
+cp ~/.config/waybar/themes/latte.css ~/.config/waybar/style.css
+# Wallpaper
+cp ~/wallpapers/ghost-carrot-catppuccin-latte.png ~/wallpapers/wallpaper.png
+# Sway
+sed -i 's/include themes\/macchiato/include themes\/latte/' ~/.config/sway/config && swaymsg reload
+# kitty
+sed -i 's/include themes\/macchiato.conf/include themes\/latte.conf/' ~/.config/kitty/kitty.conf && kitty @ set-colors --all --configured ~/.config/kitty/kitty.conf
+# fish
+cp ~/.config/fish/mythemes/latte.fish ~/.config/fish/conf.d/theme.fish
+# LunarVim
+sed -i 's/vim.g.catppuccin_flavour \= "macchiato"/vim.g.catppuccin_flavour = "latte"/' ~/.config/lvim/config.lua
+# mako
+cp ~/.config/mako/latte ~/.config/mako/config && makoctl reload
+# wofi
+cp ~/.config/wofi/macchiato.css ~/.config/wofi/style.css
