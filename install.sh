011083ec784b2233bf912ad8634b65b4681a71ca
astsu <you@example.com>
2022-08-27 17:27:25 +0300

    Update installation script.

--- install.sh
+++ install.sh
@@ -1,2 +1,38 @@
 sudo pacman -Sy
-sudo pacman -S fish kitty pipewire pipewire-alsa pipewire-pulse pipewire-jack playerctl pavucontrol polkit polkit-gnome sway swaybg swaylock waybar wofi mako otf-fontawesome ttf-fira-mono ttf-jetbrains-mono ttf-nerd-fonts-symbols xdg-desktop-portal xdg-desktop-portal-wlr btop ranger thunar slurp grim bat zip unzip openssh git
+sudo pacman -S \
+	mesa libva-mesa-driver mesa-vdpau \
+	pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol \
+	polkit polkit-gnome \
+	xorg-xwayland sway swaybg swaylock waybar wofi mako \
+	otf-font-awesome ttf-fira-mono ttf-jetbrains-mono ttf-nerd-fonts-symbols noto-fonts-emoji \
+	xdg-desktop-portal xdg-desktop-portal-wlr xdg-utils \
+	fish kitty btrfs-progs flatpak \
+	btop ranger thunar slurp grim bat \
+	zip unzip openssh git mpv jq
+
+# mesa - video driver
+# libva-mesa-driver mesa-vdpau - hardware acceleration
+# pipewire pipewire-alsa pipewire-pulse pipewire-jack - audio
+# pavucontrol - gui for controlling audio
+# polkit polkit-gnome - policy agent, policy agent gui
+# sway swaybg swaylock - wm packages
+# waybar - bar
+# wofi - launcher
+# mako - notifications daemon
+# otf-font-awesome ttf-fira-mono ttf-jetbrains-mono ttf-nerd-fonts-symbols, noto-fonts-emoji - fonts
+# fish - shell
+# kitty - terminal
+# btrfs-progs - btrfs utils
+# flatpak - flatpak package manager
+# btop - better htop
+# ranger - tui file manager
+# thunar - gui file manager
+# slurp grim - screenshots
+# bat - better cat
+# zip unzip - zip archive utils
+# openssh - ssh utils
+# git - control version system
+# mpv - video player
+# jq - json processor
+
+flatpak install flathub org.mozilla.firefox com.github.Eloston.UngoogledChromium org.telegram.desktop com.github.tchx84.Flatseal
