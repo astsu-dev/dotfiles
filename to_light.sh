c66ddf1705474a45eacca0d24614caded29ef4ed
astsu <you@example.com>
2022-09-12 19:42:43 +0300

    Add theme switcher for rofi.

--- to_light.sh
+++ to_light.sh
@@ -2,21 +2,33 @@
 
 # Waybar
 sed -i 's/macchiato.css/latte_mod.css/' ~/.config/waybar/style.css
+
 # Wallpaper
 cp ~/wallpapers/ghost-carrot-catppuccin-latte.png ~/wallpapers/wallpaper.png
+
 # Sway
 sed -i 's/include themes\/macchiato/include themes\/latte/' ~/.config/sway/config && swaymsg reload
+
 # kitty
-sed -i 's/include themes\/macchiato.conf/include themes\/latte.conf/' ~/.config/kitty/kitty.conf
+# sed -i 's/include themes\/macchiato.conf/include themes\/latte.conf/' ~/.config/kitty/kitty.conf
+
 # Alacritty
 sed -i 's/colors: \*macchiato/colors: \*latte/' ~/.config/alacritty/themes/catppuccin.yml
+
 # fish
 cp ~/.config/fish/mythemes/latte.fish ~/.config/fish/conf.d/theme.fish
+
 # LunarVim
 sed -i 's/vim.g.catppuccin_flavour = "macchiato"/vim.g.catppuccin_flavour = "latte"/' ~/.config/lvim/config.lua
+
 # mako
 cp ~/.config/mako/latte ~/.config/mako/config && makoctl reload
+
 # wofi
-cp ~/.config/wofi/themes/latte.css ~/.config/wofi/style.css
+# cp ~/.config/wofi/themes/latte.css ~/.config/wofi/style.css
+
+# rofi
+sed -i 's/macchiato/latte/' ~/.config/rofi/launcher/config.rasi && sed -i 's/macchiato/latte/' ~/.config/rofi/powermenu/config.rasi
+
 # bat
 sed -i 's/Catppuccin-macchiato/Catppuccin-latte/' ~/.config/bat/config
