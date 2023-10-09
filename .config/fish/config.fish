9f6a88ceacfaee082d314125b014bac482ffd091
astsu <you@example.com>
2023-10-09 10:35:09 +0300

    Update fish config for macos

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,14 @@ set -gx fish_greeting ''
 # Aliases
 alias to=cd
 alias lr=clear
-alias lock="swaylock -c 000000"
+alias ports="netstat -an | grep LISTEN"
+alias upgrade="brew list --formulae | xargs brew upgrade && brew list --cask | xargs brew upgrade --cask"
 
 # Pyenv init
-status is-login; and pyenv init --path | source
 pyenv init - | source
 
 # Nvm init
-nvm use 18.13.0 > /dev/null
+nvm use 18.16.0 > /dev/null
+
+# zoxide
+zoxide init fish | source
