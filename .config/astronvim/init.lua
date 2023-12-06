b465744ffebfd520528881d0ddb562c2357780be
astsu <you@example.com>
2023-12-06 14:15:38 +0200

    Update astronvim config

--- .config/astronvim/init.lua
+++ .config/astronvim/init.lua
@@ -1,65 +1,65 @@
 local config = {
-    colorscheme = "catppuccin-macchiato",
-    options = {
-        opt = {
-            wrap = true,
-        },
-        g = {
-            copilot_no_tab_map = true,
-            copilot_assume_mapped = true,
-        },
+  colorscheme = "catppuccin-macchiato",
+  options = {
+    opt = {
+      wrap = true,
     },
-    mappings = {
-        n = {
-            ["L"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-                desc = "Next buffer" },
-            ["H"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-                desc = "Previous buffer" },
-        },
-        i = {
-            ["<C-\\>"] = { 'copilot#Accept("")', silent = true, expr = true, noremap = true, replace_keycodes = false },
-        }
+    g = {
+      copilot_no_tab_map = true,
+      copilot_assume_mapped = true,
     },
-    lsp = {
-        formatting = {
-            disabled = {
-                "tsserver",
-                "html",
-            },
-        },
+  },
+  mappings = {
+    n = {
+      ["L"] = {
+        function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        desc = "Next buffer",
+      },
+      ["H"] = {
+        function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        desc = "Previous buffer",
+      },
     },
-    plugins = {
-        {
-            "catppuccin/nvim",
-            as = "catppuccin",
-            lazy = false,
-            config = function()
-                require("catppuccin").setup { transparent_background = true }
-            end,
-        },
-        {
-            "ray-x/lsp_signature.nvim",
-            event = "BufRead",
-            config = function()
-                require("lsp_signature").setup()
-            end,
-        },
-        {
-            "iamcco/markdown-preview.nvim",
-            ft = "markdown",
-            build = function()
-              vim.fn["mkdp#util#install"]()
-            end,
-        },
-        {
-            "rcarriga/nvim-notify",
-            enabled = false,
-        },
-        {
-            "github/copilot.vim",
-            lazy = false,
-        }
-    }
+    i = {
+      ["<C-\\>"] = { 'copilot#Accept("")', silent = true, expr = true, noremap = true, replace_keycodes = false },
+    },
+  },
+  lsp = {
+    formatting = {
+      disabled = {
+        "tsserver",
+        "html",
+        "jsonls",
+        "lua_ls",
+      },
+    },
+  },
+  plugins = {
+    {
+      "catppuccin/nvim",
+      as = "catppuccin",
+      lazy = false,
+      config = function() require("catppuccin").setup { transparent_background = true } end,
+    },
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "BufRead",
+      config = function() require("lsp_signature").setup() end,
+    },
+    {
+      "iamcco/markdown-preview.nvim",
+      ft = "markdown",
+      build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+      "rcarriga/nvim-notify",
+      enabled = false,
+    },
+    {
+      "github/copilot.vim",
+      lazy = false,
+    },
+  },
 }
 
 return config
