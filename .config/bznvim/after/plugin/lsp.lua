6b403a1adc077def205a37ba3b69c4c648f6cff8
astsu <you@example.com>
2024-07-20 14:49:15 +0300

    Update bznvim config

--- .config/bznvim/after/plugin/lsp.lua
+++ .config/bznvim/after/plugin/lsp.lua
@@ -4,86 +4,94 @@ local telescope_builtin = require("telescope.builtin")
 
 ---@diagnostic disable-next-line: unused-local
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function()
-    vim.lsp.buf.definition()
-  end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
 
-  vim.keymap.set("n", "<leader>lr", function()
-    vim.lsp.buf.rename()
-  end, opts)
+	vim.keymap.set("n", "<leader>lr", function()
+		vim.lsp.buf.rename()
+	end, opts)
 
-  vim.keymap.set("n", "K", function()
-    vim.lsp.buf.hover()
-  end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
 
-  vim.keymap.set("n", "<leader>ld", function()
-    vim.diagnostic.open_float()
-  end, opts)
+	vim.keymap.set("n", "<leader>ld", function()
+		vim.diagnostic.open_float()
+	end, opts)
 
-  vim.keymap.set("n", "<leader>lD", function()
-    telescope_builtin.diagnostics({ bufnr = 0 })
-  end, opts)
+	vim.keymap.set("n", "<leader>lD", function()
+		telescope_builtin.diagnostics({ bufnr = 0 })
+	end, opts)
 
-  vim.keymap.set("n", "<leader>la", function()
-    vim.lsp.buf.code_action()
-  end, opts)
+	vim.keymap.set("n", "<leader>la", function()
+		vim.lsp.buf.code_action()
+	end, opts)
 
-  vim.keymap.set("n", "<leader>lf", function()
-    vim.lsp.buf.format({ async = true })
-  end, opts)
+	vim.keymap.set("n", "<leader>lf", function()
+		vim.lsp.buf.format({ async = true })
+	end, opts)
 
-  vim.keymap.set("n", "<leader>lR", function()
-    telescope_builtin.lsp_references()
-  end, opts)
+	vim.keymap.set("n", "<leader>lR", function()
+		telescope_builtin.lsp_references()
+	end, opts)
+
+	vim.keymap.set("i", "<C-K>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+
+	vim.keymap.set("n", "<C-K>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
 local disable_client_formatting = function(client)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentFormattingRangeProvider = false
+	client.server_capabilities.documentFormattingProvider = false
+	client.server_capabilities.documentFormattingRangeProvider = false
 end
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "tsserver",
-    "pyright",
-    "ruff_lsp",
-    "eslint",
-    "lua_ls",
-    "jsonls",
-    "gopls",
-    "tailwindcss",
-    "golangci_lint_ls",
-  },
-  handlers = {
-    lsp_zero.default_setup,
-    tsserver = function()
-      lspconfig.tsserver.setup({
-        on_init = disable_client_formatting,
-      })
-    end,
-    lua_ls = function()
-      -- Use lsp-zero lua_ls configuration to disable errors in neovim lua files
-      local lua_ls_opts = lsp_zero.nvim_lua_ls()
-      lspconfig.lua_ls.setup(lua_ls_opts)
-    end,
-    -- Comment this to enable eslint lsp
-    -- eslint = function()
-    --   lspconfig.eslint.setup({
-    --     handlers = {
-    --       ["textDocument/publishDiagnostics"] = function() end,
-    --     }
-    --   })
-    -- end,
-  },
+	ensure_installed = {
+		"tsserver",
+		"pyright",
+		"ruff_lsp",
+		"eslint",
+		"lua_ls",
+		"jsonls",
+		"gopls",
+		"tailwindcss",
+		"golangci_lint_ls",
+	},
+	handlers = {
+		lsp_zero.default_setup,
+		tsserver = function()
+			lspconfig.tsserver.setup({
+				on_init = disable_client_formatting,
+			})
+		end,
+		lua_ls = function()
+			-- Use lsp-zero lua_ls configuration to disable errors in neovim lua files
+			local lua_ls_opts = lsp_zero.nvim_lua_ls()
+			lspconfig.lua_ls.setup(lua_ls_opts)
+		end,
+		-- Comment this to enable eslint lsp
+		-- eslint = function()
+		-- 	lspconfig.eslint.setup({
+		-- 		handlers = {
+		-- 			["textDocument/publishDiagnostics"] = function() end,
+		-- 		},
+		-- 	})
+		-- end,
+	},
 })
 
 local null_ls = require("null-ls")
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.stylua,
-  },
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+	},
 })
--- .config/bznvim/after/plugin/treesitter.lua
+++ .config/bznvim/after/plugin/treesitter.lua
@@ -3,19 +3,19 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
 -- Adds filetype recognition for .tact files
 parser_config.tact = {
-  install_info = {
-    url = "~/.local/git/tree-sitter-tact", -- a path to the cloned repo
-    files = {"src/parser.c"},
-    branch = "main",
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
-  }
+	install_info = {
+		url = "~/.local/git/tree-sitter-tact", -- a path to the cloned repo
+		files = { "src/parser.c" },
+		branch = "main",
+		generate_requires_npm = false,
+		requires_generate_from_grammar = false,
+	},
 }
 -- Adds filetype recognition for .tact files
 vim.filetype.add({
-  extension = {
-    tact = "tact",
-  }
+	extension = {
+		tact = "tact",
+	},
 })
 
 configs.setup({
@@ -32,7 +32,8 @@ configs.setup({
 		"css",
 		"toml",
 		"go",
-    "tact",
+		"tact",
+		"vimdoc",
 	},
 	auto_install = false,
 	sync_install = false,
