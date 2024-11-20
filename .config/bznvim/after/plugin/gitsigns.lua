2f00db683feb6eda176aad54c7d1d2ab20abe44c
astsu <you@example.com>
2024-11-20 12:09:43 +0200

    Update

--- .config/bznvim/after/plugin/gitsigns.lua
+++ .config/bznvim/after/plugin/gitsigns.lua
@@ -1,8 +1,9 @@
 local gitsigns = require("gitsigns")
 
 gitsigns.setup({
-  on_attach = function (bufnr)
-    local opts = { buffer = bufnr }
-    vim.keymap.set("n", "<leader>gl", gitsigns.blame_line, opts)
-  end
+	on_attach = function(bufnr)
+		local opts = { buffer = bufnr }
+		vim.keymap.set("n", "<leader>gl", gitsigns.blame_line, opts)
+		vim.keymap.set("n", "<leader>gd", gitsigns.diffthis, opts)
+	end,
 })
--- .config/bznvim/after/plugin/neotree.lua
+++ .config/bznvim/after/plugin/neotree.lua
@@ -1,11 +1,11 @@
 local neotree = require("neo-tree")
 
 neotree.setup({
-  filesystem = {
-    follow_current_file = {
-      enabled = true
-    }
-  }
+	filesystem = {
+		follow_current_file = {
+			enabled = true,
+		},
+	},
 })
 
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree left toggle<CR>")
